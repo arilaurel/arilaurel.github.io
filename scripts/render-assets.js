8f477bfd9ad0d2591bbcd0bd743605d3fff040b3
David Langevin <dlangevi@uwaterloo.ca>
2022-04-18 10:07:51 -0700

    improve pictures

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -5,9 +5,12 @@ export default function renderAssets() {
   const sourcePath = upath.resolve('./src/assets');
   const destPath = upath.resolve('./docs/.');
 
+  const cssAssetsDestPath = upath.resolve('./docs/css/assets');
+
   const cname = upath.resolve('./src/CNAME');
   const cnameDest = upath.resolve('./docs/CNAME');
 
   sh.cp('-R', sourcePath, destPath);
+  sh.cp('-R', sourcePath, cssAssetsDestPath);
   sh.cp(cname, cnameDest);
 };
